b4f8fa53d0663bccdcf04dedc1e5c5b76c977ac1
bretleasure <bret.leasure@outlook.com>
2020-05-01 22:00:20 -0500

    Inv2021 update and restructure folder structure

---
+++ SheetList/AddinGlobal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inventor;
+using CAP.Utilities;
+
+namespace SheetList
+{
+	public class AddinGlobal
+	{
+		public static Inventor.Application InventorApp;
+
+		public static DrawingDocument oDwgDoc;
+
+		public static string AppFolder = Tools.GetAppFolder("SheetList");
+
+		public static string SettingsFile = Tools.GetHexString("SheetList Settings") + ".xml";
+
+		public static string AppId = "5342673156831821071";
+
+		public static SheetList_Settings AppSettings;
+
+		
+
+	}
+}
