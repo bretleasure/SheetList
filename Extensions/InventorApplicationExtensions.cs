88ef2d0b1ea40f137afdf74f2aa89e2ba89a65ea
Bret Leasure <bret.leasure@outlook.com>
2023-08-01 13:13:03 -0500

    Added Inventor application extension method to get addin automation

---
+++ Extensions/InventorApplicationExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Inventor;
+
+namespace SheetList.Extensions
+{
+    public static class InventorApplicationExtensions
+    {
+        public static dynamic GetSheetListAddin(this Application inventor)
+        {
+            var addin = inventor.ApplicationAddIns.ItemById["{3dc3068f-5de4-4b85-9efe-44b7ece560f3}"];
+
+            if (addin != null)
+            {
+                addin.Activate();
+
+                return addin.Automation;
+            }
+            else
+                return null;
+        }
+    }
+}
